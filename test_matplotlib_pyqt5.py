0f02192d2e418ebbdb70f7d66566a8e469864648
Muhammad Yasirroni <48709672+yasirroni@users.noreply.github.com>
2022-06-16 09:41:59 +0700

    change import order

--- test_matplotlib_pyqt5.py
+++ test_matplotlib_pyqt5.py
@@ -1,10 +1,9 @@
 import sys
+import matplotlib
+matplotlib.use('Qt5Agg')
 from PyQt5.QtCore import QT_VERSION_STR
 from PyQt5.Qt import PYQT_VERSION_STR
 from sip import SIP_VERSION_STR
-import matplotlib
-matplotlib.use('Qt5Agg')
-
 
 print(f'Python version: {sys.version}')
 print(f'matplotlib version: {matplotlib.__version__}')
