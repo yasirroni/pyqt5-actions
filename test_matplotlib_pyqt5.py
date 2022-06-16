f74fd28ccbc048b0e6259ac72cd4459665612498
Muhammad Yasirroni <48709672+yasirroni@users.noreply.github.com>
2022-06-16 09:38:04 +0700

    print versions

--- test_matplotlib_pyqt5.py
+++ test_matplotlib_pyqt5.py
@@ -1,6 +1,13 @@
+import sys
+from PyQt5.QtCore import QT_VERSION_STR
+from PyQt5.Qt import PYQT_VERSION_STR
+from sip import SIP_VERSION_STR
 import matplotlib
 matplotlib.use('Qt5Agg')
-from matplotlib import pyplot as plt
-from PyQt5 import QtCore, QtWidgets
-import sys
-print(sys.version)
+
+
+print(f'Python version: {sys.version}')
+print(f'matplotlib version: {matplotlib.__version__}')
+print(f'Qt version:{QT_VERSION_STR}')
+print(f'SIP version:{SIP_VERSION_STR}')
+print(f'PyQt version:{PYQT_VERSION_STR}')
